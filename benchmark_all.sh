a15b238f83c316900517b2e1efabd40bf7c7556b
Stefan Schweter <stefan@schweter.it>
2018-03-01 14:11:04 +0100

    benchmark: fix main benchmark script

--- benchmark_all.sh
+++ benchmark_all.sh
@@ -1,6 +1,11 @@
-ARCHITECTURES="lstm"
+LANGUAGES=$(ls data/*.sentences.train)
+ARCHITECTURES="lstm bi-lstm cnn"
 
 for architecture in $ARCHITECTURES
 do
-  ./benchmark_language.sh $architecture
+  for train_file in $LANGUAGES
+  do
+    lang=${train_file%.train}
+    ./benchmark_language.sh $lang $architecture
+  done
 done
