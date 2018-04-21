49ee1ec50caae0858f9cf135c9814df004829db3
Stefan Schweter <stefan@schweter.it>
2018-04-22 00:19:33 +0200

    benchmark: minor formatting fixes

--- benchmark_language_ud.sh
+++ benchmark_language_ud.sh
@@ -6,7 +6,10 @@ architecture=$5
 
 function train {
   echo "Training on ${train_file} for ${architecture}"
-  python3 main.py train --training-file=${train_file} --architecture=${architecture} --model-filename=${lang_prefix}_${architecture}.model --vocab-filename=${lang_prefix}_${architecture}.vocab
+  python3 main.py train --training-file=${train_file} \
+                        --architecture=${architecture} \
+                        --model-filename=${lang_prefix}_${architecture}.model \
+                        --vocab-filename=${lang_prefix}_${architecture}.vocab
 }
 
 function prepare_dataset {
@@ -22,7 +25,10 @@ function tagging {
   # $1 dataset filename
   # $2 unique dataset name
   echo "Tagging $1"
-  python3 main.py tag --input-file $1.modified --model-filename=${lang_prefix}_${architecture}.model --vocab-filename=${lang_prefix}_${architecture}.vocab > ${lang_prefix}_${architecture}_$2_output.txt
+  python3 main.py tag --input-file $1.modified \
+                      --model-filename=${lang_prefix}_${architecture}.model \
+                      --vocab-filename=${lang_prefix}_${architecture}.vocab \
+                      > ${lang_prefix}_${architecture}_$2_output.txt
 }
 
 function final_evaluation {
@@ -30,13 +36,13 @@ function final_evaluation {
   # $1 dataset filename
   # $2 unique dataset name
   echo "Evaluation on $1 set"
-  python3 eos-eval/eval.py -g $1.gold -s ${lang_prefix}_${architecture}_$2_output.txt | tee ${lang_prefix}_${architecture}_$2_evaluation.txt
+  python3 eos-eval/eval.py -g $1.gold \
+                           -s ${lang_prefix}_${architecture}_$2_output.txt \
+                           | tee ${lang_prefix}_${architecture}_$2_evaluation.txt
 }
 
 # Disable some TensorFlow warnings and information output
 export TF_CPP_MIN_LOG_LEVEL=3
-
-echo "Lang prefix: $lang_prefix"
 
 train
 prepare_dataset $devel_file "dev"
