bc557403efe78be44503dfda342ac79df08ec3e5
Stefan Schweter <stefan@schweter.it>
2018-08-15 14:25:59 +0200

    eos: apply prospector suggestions

--- eos.py
+++ eos.py
@@ -1,3 +1,4 @@
+"""Implement a sentence boundary detection system (deep-eos)."""
 import numpy as np
 from keras.callbacks import ModelCheckpoint
 from keras.layers import Conv1D, GlobalMaxPooling1D
@@ -12,11 +13,14 @@ from keras.utils import plot_model
 from utils import Utils
 
 
-class EOS(object):
+class EOS():
+    """Implementation of the end-of-sentence classification logic."""
+
     def __init__(self):
+        """Implement the EOS constructor."""
         self.util = Utils()
 
-    def train(self, training_file,
+    def train(self, training_file,  # pylint: disable-msg=too-many-arguments,too-many-locals,too-many-statements # noqa: E501
               architecture="cnn",
               window_size=4,
               epochs=5,
@@ -30,13 +34,13 @@ class EOS(object):
               mlp_dense_units=16,
               kernel_size=5,
               filters=64,
-              pool_size=2,
               hidden_dims=250,
               strides=1,
               model_filename='best_model.hdf5',
               vocab_filename='vocab.dump'):
-        with open(training_file, mode='r', encoding='utf-8') as f:
-            training_corpus = f.read()
+        """Train a classifier for end-of-sentence detection."""
+        with open(training_file, mode='r', encoding='utf-8') as f_p:
+            training_corpus = f_p.read()
 
         data_set_char = self.util.build_data_set_char(
             training_corpus, window_size)
@@ -65,7 +69,7 @@ class EOS(object):
                              kernel_size,
                              padding='valid',
                              activation='relu',
-                             strides=1))
+                             strides=strides))
             # we use max pooling:
             model.add(GlobalMaxPooling1D())
 
@@ -118,7 +122,7 @@ class EOS(object):
                                 embedding_size))
 
             model.add(Bidirectional(
-                GRU(lstm_gru_size, dropout=dropout, recurrent_dropout=dropout)))
+                GRU(lstm_gru_size, dropout=dropout, recurrent_dropout=dropout))) # noqa: E501
 
             model.add(Dense(1, activation='sigmoid'))
 
@@ -128,7 +132,7 @@ class EOS(object):
             model.add(Activation('selu'))
             model.add(AlphaDropout(dropout))
 
-            for i in range(mlp_dense - 1):
+            for _ in range(mlp_dense - 1):
                 model.add(
                     Dense(
                         mlp_dense_units,
@@ -157,13 +161,14 @@ class EOS(object):
 
         self.util.save_vocab(char_2_id_dict, vocab_filename)
 
-    def test(self, test_file,
+    def test(self, test_file,  # pylint: disable-msg=too-many-arguments,too-many-locals # noqa: E501
              model_filename='best_model.hdf5',
              vocab_filename='vocab.dump',
              window_size=4,
              batch_size=32):
-        with open(test_file, mode='r', encoding='utf-8') as f:
-            test_corpus = f.read()
+        """Test a classifier for end-of-sentence detection."""
+        with open(test_file, mode='r', encoding='utf-8') as f_p:
+            test_corpus = f_p.read()
 
         char_2_id_dict = self.util.load_vocab(vocab_filename)
 
@@ -181,18 +186,19 @@ class EOS(object):
 
         print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1] * 100))
 
-    def tag(self, input_file,
+    def tag(self, input_file,  # pylint: disable-msg=too-many-arguments,too-many-locals # noqa: E501
             model_filename='best_model.hdf5',
             vocab_filename='vocab.dump',
             window_size=4,
             batch_size=32,
             eos_marker="</eos>"):
+        """Tag an input file with end-of-sentence markers."""
         char_2_id_dict = self.util.load_vocab(vocab_filename)
 
         model = load_model(model_filename)
 
-        with open(input_file, mode='r', encoding='utf-8') as f:
-            t = f.read()
+        with open(input_file, mode='r', encoding='utf-8') as f_p:
+            t = f_p.read()  # pylint: disable-msg=invalid-name
 
         potential_eos_list = self.util.build_potential_eos_list(t, window_size)
 
@@ -205,8 +211,8 @@ class EOS(object):
                                                 char_2_id_dict,
                                                 window_size)
 
-            if len(data_set) > 0:
-                label, feature_vector = data_set[0]
+            if data_set:
+                _, feature_vector = data_set[0]
 
                 predicted = model.predict(
                     feature_vector.reshape(
@@ -216,20 +222,18 @@ class EOS(object):
                     verbose=0)
 
                 if predicted[0][0] >= 0.5:
-                    t = t[:(eos_counter * len(eos_marker)) + start + 1] + \
-                        eos_marker + t[(eos_counter * len(eos_marker)) + start + 1:]
+                    t = t[:(eos_counter * len(eos_marker)) + start + 1] + eos_marker + t[(eos_counter * len(eos_marker)) + start + 1:]  # pylint: disable-msg=invalid-name # noqa: E501
                     eos_counter += 1
 
         print(t[:] + eos_marker)
 
     def extract(self, input_file,
-                window_size=4,
-                min_freq=10000):
-        with open(input_file, mode='r', encoding='utf-8') as f:
-            input_corpus = f.read()
+                window_size=4):
+        """Extract tabulator separated list of character window and label."""
+        with open(input_file, mode='r', encoding='utf-8') as f_p:
+            input_corpus = f_p.read()
 
         data_set_char = self.util.build_data_set_char(
             input_corpus, window_size)
 
-        print("\n".join([str(int(entry[0])) + "\t" + "".join([str(id_)
-                                                              for id_ in entry[1]]) for entry in data_set_char]))
+        print("\n".join([str(int(entry[0])) + "\t" + "".join([str(id_) for id_ in entry[1]]) for entry in data_set_char]))  # noqa: E501
