0cd1116302380bf195c4a74ebacdb9d3176e4ee6
Stefan Schweter <stefan@schweter.it>
2018-08-15 13:57:19 +0200

    utils: apply prospector suggestions

--- utils.py
+++ utils.py
@@ -1,3 +1,5 @@
+"""utils: Helper methods for deep-eos."""
+
 import pickle
 import re
 from collections import defaultdict
@@ -5,9 +7,11 @@ from collections import defaultdict
 import numpy as np
 
 
-class Utils(object):
-    def build_char_2_id_dict(self, data_set_char, min_freq):
-        """ Builds a char_to_id dictionary
+class Utils():
+    """utils: implement various helper methods for deep-eos."""
+
+    def build_char_2_id_dict(self, data_set_char, min_freq):  # pylint: disable-msg=no-self-use # noqa: E501
+        """Build a char_to_id dictionary.
 
         This methods builds a frequency list of all chars in the data set.
 
@@ -16,7 +20,8 @@ class Utils(object):
 
         # Arguments
           data_set_char: The input data set (consisting of char sequences)
-          min_freq     : Defines the minimum frequecy a char must appear in data set
+          min_freq     : Defines the minimum frequency a char must appear in
+                         data set
 
         # Returns
           char_2_id dictionary
@@ -29,14 +34,14 @@ class Utils(object):
 
         id_counter = 1
 
-        for k, v in [(k, v) for k, v in char_freq.items() if v >= min_freq]:
+        for k, v in [(k, v) for k, v in char_freq.items() if v >= min_freq]:  # pylint: disable-msg=invalid-name # noqa: E501
             char_2_id_table[k] = id_counter
             id_counter += 1
 
         return char_2_id_table
 
-    def build_data_set(self, data_set_char, char_2_id_dict, window_size):
-        """ Builds a "real" data set with numpy compatible feature vectors
+    def build_data_set(self, data_set_char, char_2_id_dict, window_size): # pylint: disable-msg=no-self-use # noqa: E501
+        """Build a "real" data set with numpy compatible feature vectors.
 
         This method converts the data_set_char to real numpy compatible feature
         vectors. It does also length checks of incoming and outgoing feature
@@ -50,7 +55,6 @@ class Utils(object):
         # Returns
           A data set which contains numpy compatible feature vectors
         """
-
         data_set = []
 
         for label, char_sequence in data_set_char:
@@ -64,14 +68,15 @@ class Utils(object):
                         ids.append(0)
 
                 feature_vector = np.array([float(ids[i])
-                                           for i in range(0, len(ids))], dtype=float)
+                                           for i in range(0, len(ids))],
+                                          dtype=float)
 
                 data_set.append((float(label), feature_vector))
 
         return data_set
 
-    def build_data_set_char(self, t, window_size):
-        """ Builds data set from corpus
+    def build_data_set_char(self, t, window_size): # pylint: disable-msg=no-self-use # noqa: E501
+        """Build a data set from corpus.
 
         This method builds a dataset from the training corpus
 
@@ -82,21 +87,20 @@ class Utils(object):
         # Returns
           A data set which contains char sequences as feature vectors
         """
-
         data_set_char_eos = \
             [(1.0, t[m.start() - window_size:m.start()].replace("\n", " ") +
               t[m.start():m.start() + window_size + 1].replace("\n", " "))
-             for m in re.finditer('[\.:?!;][^\n]?[\n]', t)]
+             for m in re.finditer(r'[\.:?!;][^\n]?[\n]', t)]
 
         data_set_char_neos = \
             [(0.0, t[m.start() - window_size:m.start()].replace("\n", " ") +
               t[m.start():m.start() + window_size + 1].replace("\n", " "))
-             for m in re.finditer('[\.:?!;][^\s]?[ ]+', t)]
+             for m in re.finditer(r'[\.:?!;][^\s]?[ ]+', t)]
 
         return data_set_char_eos + data_set_char_neos
 
-    def build_potential_eos_list(self, t, window_size):
-        """ Builds a list of potential eos from a given text
+    def build_potential_eos_list(self, t, window_size): # pylint: disable-msg=no-self-use # noqa: E501
+        """Build a list of potential eos from a given text.
 
         This method builds a list of potential end-of-sentence positions from
         a given text.
@@ -109,15 +113,14 @@ class Utils(object):
           A list of a pair, like:
             [(1.0, "eht Iv")]
           So the first position in the pair indicates the start position for a
-          potential eos. The second position holds the extracted character sequence.
+          potential eos. The second position holds the extracted character
+          sequence.
         """
-
-        PUNCT = '[\(\)\u0093\u0094`“”\"›〈⟨〈<‹»«‘’–\'``'']*'
-        EOS = '([\.:?!;])'
+        punct = r'[\(\)\u0093\u0094`“”\"›〈⟨〈<‹»«‘’–\'``'']*'
 
         eos_positions = [(m.start())
-                         for m in re.finditer(r'([\.:?!;])(\s+' + PUNCT + '|' +
-                                              PUNCT + '\s+|[\s\n]+)', t)]
+                         for m in re.finditer(r'([\.:?!;])(\s+' + punct + '|' +
+                                              punct + r'\s+|[\s\n]+)', t)]
 
         # Lets extract 2* window_size before and after eos position and remove
         # punctuation
@@ -131,31 +134,29 @@ class Utils(object):
             cleaned_left_context = left_context
             cleaned_right_context = right_context
 
-            # cleaned_left_context = re.sub(PUNCT, '', left_context)
-            # cleaned_right_context = re.sub(PUNCT, '', right_context)
+            # cleaned_left_context = re.sub(punct, '', left_context)
+            # cleaned_right_context = re.sub(punct, '', right_context)
 
             # Also replace multiple whitespaces (use *only* one whitespace)
-            cleaned_left_context = re.sub('\s+', ' ', cleaned_left_context)
-            cleaned_right_context = re.sub('\s+', ' ', cleaned_right_context)
+            cleaned_left_context = re.sub(r'\s+', ' ', cleaned_left_context)
+            cleaned_right_context = re.sub(r'\s+', ' ', cleaned_right_context)
 
-            potential_eos_position.append((eos_position,
-                                           cleaned_left_context[-window_size:] + t[eos_position] +
-                                           cleaned_right_context[1:window_size + 1]))
+            potential_eos_position.append((eos_position, cleaned_left_context[-window_size:] + t[eos_position] + cleaned_right_context[1:window_size + 1])) # noqa: E501
 
         return potential_eos_position
 
-    def save_vocab(self, char_2_id_dict, vocab_filename):
-        """ Saves vocabulary to a file
+    def save_vocab(self, char_2_id_dict, vocab_filename): # pylint: disable-msg=no-self-use # noqa: E501
+        """Save vocabulary to a file.
 
         # Arguments
           char_2_id_dict: The char_to_id dictionary
           vocab_filename: The output filename
         """
-        with open(vocab_filename, 'wb') as f:
-            pickle.dump(char_2_id_dict, f, pickle.HIGHEST_PROTOCOL)
+        with open(vocab_filename, 'wb') as f_p:
+            pickle.dump(char_2_id_dict, f_p, pickle.HIGHEST_PROTOCOL)
 
-    def load_vocab(self, vocab_filename):
-        """ Loads vocabulary from file
+    def load_vocab(self, vocab_filename): # pylint: disable-msg=no-self-use # noqa: E501
+        """Load vocabulary from file.
 
         # Arguments
           vocab_filename: The vocabulary filename to be read in
@@ -163,5 +164,5 @@ class Utils(object):
         # Returns
           Dictionary of vocabulary
         """
-        with open(vocab_filename, 'rb') as f:
-            return pickle.load(f)
+        with open(vocab_filename, 'rb') as f_p:
+            return pickle.load(f_p)
